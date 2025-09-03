c6a04ca47df6222375eafb0a534bb57844fbad20
anna-shen-hmrc <anna-shen-hmrc@users.noreply.github.com>
2025-09-03 10:15:52 +0100

    NGR-980: address PR bot

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"                        % bootstrapVersion,
     "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"                        % "12.8.0",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                                % hmrcMongoVersion,
-    "uk.gov.hmrc"             %% "centralised-authorisation-resource-client-play-30" % "1.10.0",
+    "uk.gov.hmrc"             %% "centralised-authorisation-resource-client-play-30" % "1.11.0",
     "com.beachape"            %% "enumeratum-play"                                   % enumeratumVersion,
     "uk.gov.hmrc"             %% "domain-play-30"                                    % "11.0.0"
   )
