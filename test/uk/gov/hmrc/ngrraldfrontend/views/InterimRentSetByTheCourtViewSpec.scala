8f1e68adef6d12fc214c5f42369dc4aa95c0bb0f
JakeReid2020 <75027964+JakeReid2020@users.noreply.github.com>
2025-09-17 09:01:15 +0100

    [NGR-1126] Adding all the tests

--- test/uk/gov/hmrc/ngrraldfrontend/views/InterimRentSetByTheCourtViewSpec.scala
+++ test/uk/gov/hmrc/ngrraldfrontend/views/InterimRentSetByTheCourtViewSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.ngrraldfrontend.views
 
 import org.jsoup.Jsoup
@@ -25,8 +41,8 @@ class InterimRentSetByTheCourtViewSpec extends ViewBaseSpec {
 
   object Selectors {
     val heading = "#main-content > div > div.govuk-grid-column-two-thirds > form > h1"
-    val label1 = "#main-content > div > div.govuk-grid-column-two-thirds > form > div:nth-child(4) > h1 > label"
-    val label2 = "#main-content > div > div.govuk-grid-column-two-thirds > form > div:nth-child(5) > fieldset > legend"
+    val label1 = "#main-content > div > div.govuk-grid-column-two-thirds > form > div:nth-child(3) > h1"
+    val label2 = "#main-content > div > div.govuk-grid-column-two-thirds > form > div:nth-child(4) > fieldset > legend"
     val hint2 = "#date-hint"
     val saveAndContinue = "#continue"
   }
