df681af55c6b9f79a81a25d81324ec6b55a2ff5d
Pete Robinson <77154273+peterdrobinson@users.noreply.github.com>
2025-09-25 14:42:50 +0100

    NGR-1091 - Removed commented out code

--- test/uk/gov/hmrc/ngrraldfrontend/views/LandlordViewSpec.scala
+++ test/uk/gov/hmrc/ngrraldfrontend/views/LandlordViewSpec.scala
@@ -51,17 +51,12 @@ class LandlordViewSpec extends ViewBaseSpec {
       val radioLabel = "#main-content > div > div > form > div:nth-child(5) > fieldset > legend > h1"
       val radio1 = "#main-content > div > div > form > div:nth-child(5) > fieldset > div > div:nth-child(1) > label"
       val radio2 = "#main-content > div > div > form > div:nth-child(5) > fieldset > div > div:nth-child(2) > label"
-//      val radio3 = "#main-content > div > div > form > div:nth-child(5) > fieldset > div > div:nth-child(3) > label"
-//      val radio4 = "#main-content > div > div > form > div:nth-child(5) > fieldset > div > div:nth-child(4) > label"
-//      val radio5 = "#main-content > div > div > form > div:nth-child(5) > fieldset > div > div:nth-child(5) > label"
       val continue = "#continue"
     }
 
     val address = "5 Brixham Marina, Berry Head Road, Brixham, Devon, TQ5 9BW"
     private val landLordAndTennant: NGRRadioButtons = NGRRadioButtons("Yes", LandlordYes)
     private val familyMember: NGRRadioButtons = NGRRadioButtons("No", LandlordNo)
-//    private val companyPensionFund: NGRRadioButtons = NGRRadioButtons("Company pension fund", CompanyPensionFund)
-//    private val businessPartnerOrSharedDirector: NGRRadioButtons = NGRRadioButtons("Company pension fund", BusinessPartnerOrSharedDirector)
 
     private def otherRelationship(form: Form[LandlordForm])(implicit messages: Messages): NGRRadioButtons = NGRRadioButtons(
       radioContent = "service.yes",
