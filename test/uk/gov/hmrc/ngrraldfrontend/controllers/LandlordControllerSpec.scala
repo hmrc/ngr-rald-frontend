ee3ead61f02260b4f6445d2c7891509f2e464167
anna-shen-hmrc <anna-shen-hmrc@users.noreply.github.com>
2025-10-03 15:15:23 +0100

    NGR-1092: Landlord store true or false

--- test/uk/gov/hmrc/ngrraldfrontend/controllers/LandlordControllerSpec.scala
+++ test/uk/gov/hmrc/ngrraldfrontend/controllers/LandlordControllerSpec.scala
@@ -57,7 +57,7 @@ class LandlordControllerSpec extends ControllerSpecSupport {
         val content = contentAsString(result)
         val document = Jsoup.parse(content)
         document.select("input[name=landlord-name-value]").attr("value") mustBe "Joe Bloggs"
-        document.select("input[name=landlord-radio]").attr("value") mustBe "LandlordRelationshipYes"
+        document.select("input[name=landlord-radio][value=true]").hasAttr("checked") mustBe true
 
       }
       "Return NotFoundException when property is not found in the mongo" in {
@@ -70,12 +70,12 @@ class LandlordControllerSpec extends ControllerSpecSupport {
     }
 
     "method submit" must {
-      "Return OK and the correct view after submitting with name and radio button selected while in the rent agreement journey" in {
+      "Return SEE_OTHER and the correct view after submitting with name and radio button selected while in the rent agreement journey" in {
         when(mockSessionRepository.set(any())).thenReturn(Future.successful(true))
         val result = filledController(rentAgreementAnswers).submit(NormalMode)(AuthenticatedUserRequest(FakeRequest(routes.LandlordController.submit(NormalMode))
           .withFormUrlEncodedBody(
             "landlord-name-value" -> "Bob",
-            "landlord-radio" -> "LandlordRelationshipNo"
+            "landlord-radio" -> "false"
           )
           .withHeaders(HeaderNames.authorisation -> "Bearer 1"), None, None, None, Some(property), credId = Some(credId.value), None, None, nino = Nino(true, Some(""))))
         result.map(result => {
@@ -84,27 +84,13 @@ class LandlordControllerSpec extends ControllerSpecSupport {
         status(result) mustBe SEE_OTHER
         redirectLocation(result) mustBe Some(routes.WhatIsYourRentBasedOnController.show(NormalMode).url)
       }
-      "Return OK and the correct view after submitting with name and radio button selected while in the renewedAgreement journey" in {
-        when(mockSessionRepository.set(any())).thenReturn(Future.successful(true))
-        val result = filledController(renewedAgreementAnswers).submit(NormalMode)(AuthenticatedUserRequest(FakeRequest(routes.LandlordController.submit(NormalMode))
-          .withFormUrlEncodedBody(
-            "landlord-name-value" -> "Bob",
-            "landlord-radio" -> "LandlordRelationshipNo"
-          )
-          .withHeaders(HeaderNames.authorisation -> "Bearer 1"), None, None, None, Some(property), credId = Some(credId.value), None, None, nino = Nino(true, Some(""))))
-        result.map(result => {
-          result.header.headers.get("Location") mustBe Some("/ngr-rald-frontend/landlord")
-        })
-        status(result) mustBe SEE_OTHER
-        redirectLocation(result) mustBe Some(routes.WhatTypeOfAgreementController.show(NormalMode).url)
-      }
-      "Return OK and the correct view after submitting with name and other radio button selected with description added while in the new agreement journey" in {
+      "Return SEE_OTHER and the correct view after submitting with name and other radio button selected with description added while in the new agreement journey" in {
         when(mockSessionRepository.set(any())).thenReturn(Future.successful(true))
         val result = filledController(newAgreementAnswers).submit(NormalMode)(AuthenticatedUserRequest(FakeRequest(routes.LandlordController.submit(NormalMode))
           .withFormUrlEncodedBody(
             "landlord-name-value" -> "Bob",
-            "landlord-radio" -> "LandLordAndTenant",
-            "landlordOther" -> "Description of other",
+            "landlord-radio" -> "true",
+            "landlord-relationship" -> "Description of relationship",
           )
           .withHeaders(HeaderNames.authorisation -> "Bearer 1"), None, None, None, Some(property), credId = Some(credId.value), None, None, nino = Nino(true, Some(""))))
         result.map(result => {
@@ -117,7 +103,7 @@ class LandlordControllerSpec extends ControllerSpecSupport {
         val result = controllerProperty.submit(NormalMode)(AuthenticatedUserRequest(FakeRequest(routes.LandlordController.submit(NormalMode))
           .withFormUrlEncodedBody(
             "landlord-name-value" -> "",
-            "landlord-radio" -> "LandLordAndTenant"
+            "landlord-radio" -> "false"
           )
           .withHeaders(HeaderNames.authorisation -> "Bearer 1"), None, None, None, Some(property), credId = Some(credId.value), None, None, nino = Nino(true, Some(""))))
         result.map(result => {
@@ -126,6 +112,7 @@ class LandlordControllerSpec extends ControllerSpecSupport {
         status(result) mustBe BAD_REQUEST
         val content = contentAsString(result)
         content must include(pageTitle)
+        content must include("<a href=\"#landlord-name-value\">Enter the landlord's full name</a>")
       }
       "Return Form with Errors when no radio button is selected" in {
         val result = controllerProperty.submit(NormalMode)(AuthenticatedUserRequest(FakeRequest(routes.LandlordController.submit(NormalMode))
@@ -140,13 +127,14 @@ class LandlordControllerSpec extends ControllerSpecSupport {
         status(result) mustBe BAD_REQUEST
         val content = contentAsString(result)
         content must include(pageTitle)
+        content must include("<a href=\"#landlord-radio\">Select yes if you have any relationship with landlord</a>")
       }
       "Return Form with Errors when yes radio button is selected with no text" in {
         val result = controllerProperty.submit(NormalMode)(AuthenticatedUserRequest(FakeRequest(routes.LandlordController.submit(NormalMode))
           .withFormUrlEncodedBody(
             "landlord-name-value" -> "Bob",
-            "landlord-radio" -> "LandlordRelationshipYes",
-            "landlordOther" -> "",
+            "landlord-radio" -> "true",
+            "landlord-relationship" -> ""
           )
           .withHeaders(HeaderNames.authorisation -> "Bearer 1"), None, None, None, Some(property), credId = Some(credId.value), None, None, nino = Nino(true, Some(""))))
         result.map(result => {
@@ -155,16 +143,20 @@ class LandlordControllerSpec extends ControllerSpecSupport {
         status(result) mustBe BAD_REQUEST
         val content = contentAsString(result)
         content must include(pageTitle)
+        content must include("<a href=\"#landlord-relationship\">Tell us what your relationship with the landlord is</a>")
       }
-//      "Return Exception if no address is in the mongo" in {
-//        mockRequestWithoutProperty()
-//        val exception = intercept[NotFoundException] {
-//          await(controller.submit(NormalMode)(AuthenticatedUserRequest(FakeRequest(routes.LandlordController.submit(NormalMode))
-//            .withFormUrlEncodedBody(("what-type-of-agreement-radio", ""))
-//            .withHeaders(HeaderNames.authorisation -> "Bearer 1"), None, None, None, Some(property), credId = Some(credId.value), None, None, nino = Nino(true, Some("")))))
-//        }
-//        exception.getMessage contains "Couldn't find property in mongo" mustBe true
-//      }
+      "Return Exception if no address is in the mongo" in {
+        val exception = intercept[NotFoundException] {
+          await(controllerNoProperty.submit(NormalMode)(AuthenticatedUserRequest(FakeRequest(routes.LandlordController.submit(NormalMode))
+            .withFormUrlEncodedBody(
+              "landlord-name-value" -> "Bob",
+              "landlord-radio" -> "true",
+              "landlord-relationship" -> ""
+            )
+            .withHeaders(HeaderNames.authorisation -> "Bearer 1"), None, None, None, Some(property), credId = Some(credId.value), None, None, nino = Nino(true, Some("")))))
+        }
+        exception.getMessage contains "Could not find answers in backend mongo" mustBe true
+      }
     }
   }
 }
