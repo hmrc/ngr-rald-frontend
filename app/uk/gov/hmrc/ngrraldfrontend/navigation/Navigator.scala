0e35c5ed1cc2e7a75675053d6705b094cb49726b
JakeReid2020 <75027964+JakeReid2020@users.noreply.github.com>
2025-10-07 09:54:59 +0100

    [NGR-1324] Rebase

--- app/uk/gov/hmrc/ngrraldfrontend/navigation/Navigator.scala
+++ app/uk/gov/hmrc/ngrraldfrontend/navigation/Navigator.scala
@@ -142,8 +142,8 @@ class Navigator @Inject()() {
     case DoYouPayExtraForParkingSpacesPage => answers =>
       answers.get(DoYouPayExtraForParkingSpacesPage) match {
         case Some(value) => value match {
-          case true => uk.gov.hmrc.ngrraldfrontend.controllers.routes.DoYouPayExtraForParkingSpacesController.show(NormalMode)
-          case _    => uk.gov.hmrc.ngrraldfrontend.controllers.routes.RepairsAndInsuranceController.show(NormalMode)
+          case true => uk.gov.hmrc.ngrraldfrontend.controllers.routes.ParkingSpacesOrGaragesNotIncludedInYourRentController.show(NormalMode)
+          case _     => uk.gov.hmrc.ngrraldfrontend.controllers.routes.RepairsAndInsuranceController.show(NormalMode)
         }
         case None => throw new NotFoundException("Failed to find answers")
       }
