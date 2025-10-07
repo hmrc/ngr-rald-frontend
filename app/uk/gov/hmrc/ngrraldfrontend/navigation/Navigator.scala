18e46e56b57d34b4f132d3ae31d8686a04e175c2
Pete Robinson <77154273+peterdrobinson@users.noreply.github.com>
2025-10-07 15:20:13 +0100

    NGR-1538 - Added note to Nav

--- app/uk/gov/hmrc/ngrraldfrontend/navigation/Navigator.scala
+++ app/uk/gov/hmrc/ngrraldfrontend/navigation/Navigator.scala
@@ -156,7 +156,7 @@ class Navigator @Inject()() {
     case ConfirmBreakClausePage => _ => uk.gov.hmrc.ngrraldfrontend.controllers.routes.LandlordController.show(NormalMode) //TODO This needs to be amended when the journey is completed
     //TODO Next page not made yet
     case RentReviewPage => _ => uk.gov.hmrc.ngrraldfrontend.controllers.routes.CheckRentFreePeriodController.show(NormalMode)
-    case HowMuchWasTheLumpSumPage => _ => uk.gov.hmrc.ngrraldfrontend.controllers.routes.CheckRentFreePeriodController.show(NormalMode)
+    case HowMuchWasTheLumpSumPage => _ => uk.gov.hmrc.ngrraldfrontend.controllers.routes.CheckRentFreePeriodController.show(NormalMode) //TODO This needs to be amended when the journey is completed
   }
 
   //TODO change to check your answers page
