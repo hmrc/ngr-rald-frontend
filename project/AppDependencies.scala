a455c5ccf95441e46e6543da364ccb3e90a749b8
Paul <3755417+halpz@users.noreply.github.com>
2025-12-11 09:01:27 +0000

    update dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"                        % bootstrapVersion,
     "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"                        % "12.22.0",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                                % hmrcMongoVersion,
-    "uk.gov.hmrc"             %% "centralised-authorisation-resource-client-play-30" % "1.12.0",
+    "uk.gov.hmrc"             %% "centralised-authorisation-resource-client-play-30" % "1.15.0",
     "com.beachape"            %% "enumeratum-play"                                   % enumeratumVersion,
     "uk.gov.hmrc"             %% "domain-play-30"                                    % "11.0.0"
   )
